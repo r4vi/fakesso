0150ee1876d63028929944071cc483e0ede0192f
Ravi Kotecha <kotecha.ravi@gmail.com>
2017-11-03 22:34:09 +0000

    initial commit

---
+++ src/fakesso/service.clj
@@ -0,0 +1,102 @@
+(ns fakesso.service
+  (:require [io.pedestal.http :as http]
+            [io.pedestal.http.route :as route]
+            [io.pedestal.http.body-params :as body-params]
+            [ring.util.response :as ring-resp]
+            [ring.util.codec :as codec]
+            [environ.core :refer [env]]))
+;; utils
+
+(defn make-query-string [m & [encoding]]
+  (let [s #(if (instance? clojure.lang.Named %) (name %) %)
+        enc (or encoding "UTF-8")]
+    (->> (for [[k v] m]
+           (str (codec/url-encode (s k) enc)
+                "="
+                (codec/url-encode (str v) enc)))
+         (interpose "&")
+         (apply str))))
+
+(defn build-url [url url-params]
+  (str url "?" (make-query-string url-params)))
+
+(defn rand-str [len]
+  (apply str (take len (repeatedly #(char (+ (rand 26) 65))))))
+
+
+;; views
+(defn home-page
+  [request]
+  (ring-resp/response "If you've been redirected here you should probably have provided a 'redirect_uri' parameter to /o/authorize/"))
+
+(defn authorize [request]
+  (if-let [p (merge (:json-params request) (:params request))]
+    (ring-resp/redirect (build-url
+                         (:redirect_uri p "/")
+                         {:state (:state p "state")
+                          :code (rand-str 10)}))
+    (ring-resp/response "" 403)
+    ))
+
+(defn introspect [request]
+  (if (get-in request [:params :token])
+    (ring-resp/response {:active true
+                         :scope (get-in request [:params :scope] "read write")
+                         :exp (+ (rand-int 99999))})
+    (ring-resp/response {:active false})
+    ))
+
+(defn token [request]
+  (ring-resp/response {:access_token (rand-str 20)
+                       :scope (get-in request [:params :scope] "read write")
+                       :exp (+ (rand-int 99999))
+                       })
+  )
+
+(def html-interceptors [http/html-body])
+(def json-interceptors [http/json-body])
+
+;; Tabular routes
+(def routes #{["/" :get (conj html-interceptors `home-page)]
+              ["/o/authorize/" :post (conj json-interceptors `authorize) :route-name :authorize-post]
+              ["/o/authorize/" :get (conj json-interceptors `authorize) :route-name :authorize]
+              ["/o/introspect/" :post (conj json-interceptors `introspect) :route-name :introspect-post]
+              ["/o/introspect/" :get (conj json-interceptors `introspect) :route-name :introspect]
+              ["/o/token/" :post (conj json-interceptors `token) :route-name :token-post]
+              ["/o/token/" :get (conj json-interceptors `token) :route-name :token]
+              })
+
+;; Consumed by fakesso.server/create-server
+;; See http/default-interceptors for additional options you can configure
+(def service {:env :prod
+              ::http/routes routes
+
+              ;; Uncomment next line to enable CORS support, add
+              ;; string(s) specifying scheme, host and port for
+              ;; allowed source(s):
+              ;;
+              ;; "http://localhost:8080"
+              ;;
+              ;;::http/allowed-origins ["scheme://host:port"]
+
+              ;; Tune the Secure Headers
+              ;; and specifically the Content Security Policy appropriate to your service/application
+              ;; For more information, see: https://content-security-policy.com/
+              ;;   See also: https://github.com/pedestal/pedestal/issues/499
+              ;;::http/secure-headers {:content-security-policy-settings {:object-src "'none'"
+              ;;                                                          :script-src "'unsafe-inline' 'unsafe-eval' 'strict-dynamic' https: http:"
+              ;;                                                          :frame-ancestors "'none'"}}
+
+              ;; Root for resource interceptor that is available by default.
+              ::http/resource-path "/public"
+
+              ;; Either :jetty, :immutant or :tomcat (see comments in project.clj)
+              ;;  This can also be your own chain provider/server-fn -- http://pedestal.io/reference/architecture-overview#_chain_provider
+              ::http/type :jetty
+              ;;::http/host "localhost"
+              ::http/port (read-string (:port env "8080"))
+              ;; Options to pass to the container (Jetty)
+              ::http/container-options {:h2c? true
+                                        :h2? false
+                                        :ssl? false}})
+
---
+++ test/fakesso/service_test.clj
@@ -0,0 +1,38 @@
+(ns fakesso.service-test
+  (:require [clojure.test :refer :all]
+            [io.pedestal.test :refer :all]
+            [io.pedestal.http :as bootstrap]
+            [fakesso.service :as service]))
+
+(def service
+  (::bootstrap/service-fn (bootstrap/create-servlet service/service)))
+
+(deftest home-page-test
+  (is (=
+       (:body (response-for service :get "/"))
+       "If you've been redirected here you should probably have provided a 'redirect_uri' parameter to /o/authorize/"))
+  (is (=
+       (:headers (response-for service :get "/"))
+       {"Content-Type" "text/html;charset=UTF-8"
+        "Strict-Transport-Security" "max-age=31536000; includeSubdomains"
+        "X-Frame-Options" "DENY"
+        "X-Content-Type-Options" "nosniff"
+        "X-XSS-Protection" "1; mode=block"
+        "X-Download-Options" "noopen"
+        "X-Permitted-Cross-Domain-Policies" "none"
+        "Content-Security-Policy" "object-src 'none'; script-src 'unsafe-inline' 'unsafe-eval' 'strict-dynamic' https: http:;"})))
+
+(deftest token-test
+  (is (.contains
+       (:body (response-for service :get "/o/token/"))
+       "access_token"))
+  (is (=
+       (:headers (response-for service :get "/o/token/"))
+       {"Content-Type" "application/json;charset=UTF-8"
+        "Strict-Transport-Security" "max-age=31536000; includeSubdomains"
+        "X-Frame-Options" "DENY"
+        "X-Content-Type-Options" "nosniff"
+        "X-XSS-Protection" "1; mode=block"
+        "X-Download-Options" "noopen"
+        "X-Permitted-Cross-Domain-Policies" "none"
+        "Content-Security-Policy" "object-src 'none'; script-src 'unsafe-inline' 'unsafe-eval' 'strict-dynamic' https: http:;"})))
